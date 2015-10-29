ef08f48d8721ad8d4d7bbf13210695f666790a18
ss512824 <z0970017119@gmail.com>
2015-10-29 17:51:15 +0800

    20151029

---
+++ WindowsApplication3/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意:  以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.b1 = New System.Windows.Forms.Button()
+        Me.b2 = New System.Windows.Forms.Button()
+        Me.txtUserName = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'b1
+        '
+        Me.b1.Location = New System.Drawing.Point(75, 205)
+        Me.b1.Name = "b1"
+        Me.b1.Size = New System.Drawing.Size(75, 23)
+        Me.b1.TabIndex = 0
+        Me.b1.Text = "確定"
+        Me.b1.UseVisualStyleBackColor = True
+        '
+        'b2
+        '
+        Me.b2.Location = New System.Drawing.Point(339, 205)
+        Me.b2.Name = "b2"
+        Me.b2.Size = New System.Drawing.Size(75, 23)
+        Me.b2.TabIndex = 1
+        Me.b2.Text = "離開"
+        Me.b2.UseVisualStyleBackColor = True
+        '
+        'txtUserName
+        '
+        Me.txtUserName.BackColor = System.Drawing.Color.White
+        Me.txtUserName.Location = New System.Drawing.Point(188, 26)
+        Me.txtUserName.Name = "txtUserName"
+        Me.txtUserName.Size = New System.Drawing.Size(100, 22)
+        Me.txtUserName.TabIndex = 2
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(188, 120)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(100, 22)
+        Me.txtPassword.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(507, 329)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtUserName)
+        Me.Controls.Add(Me.b2)
+        Me.Controls.Add(Me.b1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents b1 As System.Windows.Forms.Button
+    Friend WithEvents b2 As System.Windows.Forms.Button
+    Friend WithEvents txtUserName As System.Windows.Forms.TextBox
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
+
+End Class
---
+++ WindowsApplication3/Form1.vb
@@ -0,0 +1,36 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    Private Sub b1_Click(sender As Object, e As EventArgs) Handles b1.Click
+        If (String.IsNullOrEmpty(txtUserName.Text)) Then
+            txtUserName.BackColor = Color.MistyRose
+        End If
+        If (String.IsNullOrEmpty(txtPassword.Text)) Then
+            txtPassword.BackColor = Color.MistyRose
+        End If
+
+        If txtUserName.Text = "LOGIN" Then
+            If txtPassword.Text = "ABC123" Then
+                MsgBox("帳戶密碼正確:)")
+                Application.Exit()
+            Else
+                txtPassword.BackColor = Color.MistyRose
+            End If
+        Else
+            txtUserName.BackColor = Color.MistyRose
+        End If
+        If txtUserName.Text = "LOGIN" Then
+            txtUserName.BackColor = Color.White
+        End If
+        If txtPassword.Text = "ABC123" Then
+            txtPassword.BackColor = Color.White
+        End If
+
+    End Sub
+
+    Private Sub b2_Click(sender As Object, e As EventArgs) Handles b2.Click
+        Application.Exit()
+    End Sub
+End Class
